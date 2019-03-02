b134af859c3320af80bc53cdca85df3d5294ac04
MarcServat <marcservat@hotmail.com>
2019-03-02 15:23:41 +0100

    WIP

--- App.js
+++ App.js
@@ -10,28 +10,28 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import firebase from 'firebase'
-// import {Header} from "./src/components/common";
+import {Header} from "./src/components/common";
 import LoginForm from "./src/components/LoginForm";
 
 type Props = {};
 export default class App extends Component<Props> {
 
   componentWillMount() {
-    // firebase.initializeApp({
-    //   apiKey: "AIzaSyAIgGXNbrGeNss7xoGrd8xx5RMsIar7NRg",
-    //   authDomain: "auth-7c065.firebaseapp.com",
-    //   databaseURL: "https://auth-7c065.firebaseio.com",
-    //   projectId: "auth-7c065",
-    //   storageBucket: "auth-7c065.appspot.com",
-    //   messagingSenderId: "374313026599"
-    // });
+    firebase.initializeApp({
+      apiKey: "AIzaSyAIgGXNbrGeNss7xoGrd8xx5RMsIar7NRg",
+      authDomain: "auth-7c065.firebaseapp.com",
+      databaseURL: "https://auth-7c065.firebaseio.com",
+      projectId: "auth-7c065",
+      storageBucket: "auth-7c065.appspot.com",
+      messagingSenderId: "374313026599"
+    });
   }
 
   render() {
     return (
       <View>
-        {/*<Header title="HIHIHIH"/>*/}
-        {/*<LoginForm/>*/}
+        <Header title="HIHIHIH"/>
+        <LoginForm/>
       </View>
     );
   }
--- index.js
+++ index.js
@@ -4,6 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
+import 'babel-polyfill';
 import App from './App';
 import {name as appName} from './app.json';
 
